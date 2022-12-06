5ce4c7ce2af595a249a3331490241e8594cfe639
Lisa Carrier <lisa.carrier@clever-cloud.com>
2022-12-07 00:19:00 +0100

    corrected camel case in day 3 + factorization read input

--- 03/src/main/scala/solution.scala
+++ 03/src/main/scala/solution.scala
@@ -1,53 +1,52 @@
 object solution {
 
-  def read_data(file_name: String): List[String] = {
-    scala.io.Source.fromFile(file_name).getLines.toList
+  def readData(fileName: String): List[String] = {
+    scala.io.Source.fromFile(fileName).getLines.toList
   }
 
-  def part_1() : Unit = {
-    val rucksacks = read_data("./input.txt")
-    val total_priority = map_priority(rucksacks)
-    println("Sum of all priority : " + total_priority)
+  def part1(rucksacks: List[String]) : Unit = {
+    val totalPriority = mapPriority(rucksacks)
+    println("Sum of all priority : " + totalPriority)
   }
 
-  def map_priority(rucksacks: List[String]): Int = rucksacks match {
+  def mapPriority(rucksacks: List[String]): Int = rucksacks match {
     case Nil => 0
     case rucksack :: tail => {
-      compute_priority_of_rucksack(rucksack) + map_priority(tail)
+      computePriorityOfRucksack(rucksack) + mapPriority(tail)
     }
   }
 
-  def compute_priority_of_rucksack(rucksack : String) : Int = {
-    val middle_of_rucksack = rucksack.length / 2
-    val compartments = List(rucksack.substring(middle_of_rucksack), rucksack.substring(0, middle_of_rucksack))
+  def computePriorityOfRucksack(rucksack: String) : Int = {
+    val middleOfRucksack = rucksack.length / 2
+    val compartments = List(rucksack.substring(middleOfRucksack), rucksack.substring(0, middleOfRucksack))
     val item = (compartments(0) intersect compartments(1)).charAt(0)
-    compute_priority_of_item(item)
+    computePriorityOfItem(item)
   }
 
-  def compute_priority_of_item(item : Char) : Int= {
+  def computePriorityOfItem(item: Char) : Int= {
     (('a' to 'z').toList ++ ('A' to 'Z').toList).indexOf(item) +1
   }
 
-  def part_2() = {
-    val rucksacks = read_data("./input.txt")
-    val total_priority =  map_group(rucksacks)
-    println("Sum of all priority : " + total_priority)
+  def part2(rucksacks: List[String]) = {
+    val totalPriority =  mapGroup(rucksacks)
+    println("Sum of all priority : " + totalPriority)
   }
 
-  def map_group(rucksacks: List[String]): Int = rucksacks match {
+  def mapGroup(rucksacks: List[String]): Int = rucksacks match {
     case Nil => 0
-    case group_1 :: group_2 :: group_3 :: tail => {
-      val common_item = (group_1 intersect group_2 intersect group_3).charAt(0)
-      compute_priority_of_item(common_item) + map_group(tail)
+    case group1 :: group2 :: group3 :: tail => {
+      val commonItem = (group1 intersect group2 intersect group3).charAt(0)
+      computePriorityOfItem(commonItem) + mapGroup(tail)
     }
   }
 
   def main(args: Array[String]) {
     try {
+      val rucksacks = readData("./input.txt")
       println("--- PARTIE 1 ---")
-      part_1()
+      part1(rucksacks)
       println("--- PARTIE 2 ---")
-      part_2()
+      part2(rucksacks)
     } catch {
       case e: Exception => println("Error : " + e.getMessage);
     }
